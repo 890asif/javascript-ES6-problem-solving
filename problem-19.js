a2cb0d7a3aba570225864a999e753473c3bf26b2
890asif <192508892+890asif@users.noreply.github.com>
2026-08-08 14:58:13 +0600

    truthy and falsy value

---
+++ problem-19.js
@@ -0,0 +1,10 @@
+const  isStrictMatch = (input, target)=>{
+    if(input === target){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(isStrictMatch("18", 18))
+console.log(isStrictMatch(18, 18))
