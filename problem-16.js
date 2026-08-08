5b655222bb8b21adcbc578787534a5519e8a4dfe
890asif <192508892+890asif@users.noreply.github.com>
2026-08-08 10:41:16 +0600

    closure

---
+++ problem-16.js
@@ -0,0 +1,12 @@
+const createIdGenerator = ()=>{
+    let count = 0;
+    return () =>{
+        count++
+        return count;
+    }
+}
+const nextId = createIdGenerator()
+
+console.log(nextId())
+console.log(nextId())
+console.log(nextId())
